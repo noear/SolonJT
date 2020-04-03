8eee1afbc2202fa3cc32ee72ff832946505abb02
noear <noear@live.cn>
2020-04-03 16:47:28 +0800

    1.0.4.2

--- custom/localjt/src/main/java/org/noear/localjt/LocalJtApp.java
+++ custom/localjt/src/main/java/org/noear/localjt/LocalJtApp.java
@@ -56,6 +56,24 @@ public class LocalJtApp{
             new Thread(() -> {
                 WebShell.start(args);
             }).start();
+        }else{
+            //尝试浏览器打开
+            if (java.awt.Desktop.isDesktopSupported()) {
+                try {
+                    // 创建一个URI实例
+                    java.net.URI uri = java.net.URI.create(home);
+                    // 获取当前系统桌面扩展
+                    java.awt.Desktop dp = java.awt.Desktop.getDesktop();
+                    // 判断系统桌面是否支持要执行的功能
+                    if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
+                        // 获取系统默认浏览器打开链接
+                        dp.browse(uri);
+                    }
+
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
